b7370f760b6ae15aaa03dd15212768a9452dafa8
WjrRyan <ryanwjr0115@outlook.com>
2026-03-16 20:50:32 +0800

    Add iHubMobile/AppDelegate.h

---
+++ iHubMobile/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  iHubMobile
+//
+//  Created by 王竞然 on 2026/3/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
