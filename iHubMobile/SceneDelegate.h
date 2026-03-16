016f28652bedae1a4c54f03697673c2a0dc36626
WjrRyan <ryanwjr0115@outlook.com>
2026-03-16 20:50:53 +0800

    Add iHubMobile/SceneDelegate.h

---
+++ iHubMobile/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  iHubMobile
+//
+//  Created by 王竞然 on 2026/3/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
